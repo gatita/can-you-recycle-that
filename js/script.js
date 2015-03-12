fdb119aea841c72a343faf92e5f45bc564be85cc
gatita <hatamyarg@gmail.com>
2015-03-11 17:56:13 -0700

    get form value

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
+// var e = {"wa": {
+// 	"bottle cap": {"recycle": "no", "source": "url"},
+// 	}
+// };
+
 var e = {"wa": {
-	"bottle cap": {"recycle": "no", "source": "url"},
+	"bottle cap": {"recycle": "no", "source": "url"}
 	}
 };
 
 $(document).ready(function(){
-	$("#hello").html(e.wa['bottle cap'].recycle);
+	$('#queried').click(function(){
+		var query = $('#query').val();
+		alert(query);
+	})
 });
 
